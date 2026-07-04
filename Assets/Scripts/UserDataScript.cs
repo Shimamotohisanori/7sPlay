0dd8357a4bad79dcd7c709142b6a0ee6dcafa947
Shimamoto Hisanori <ca01254009@st.kawahara.ac.jp>
2026-07-04 15:32:29 +0900

    ホール内での画面の切り替え及びUIの切り替えを実装

---
+++ Assets/Scripts/UserDataScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/** ユーザーの情報を管理するクラス */
+[CreateAssetMenu(fileName = "UserData", menuName = "Game/UserData")]
+public class UserDataScript : ScriptableObject
+{
+    /** ユーザーの名前 */
+    public string userName;
+
+    /** ユーザーのVコイン */
+    public int vcoin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
